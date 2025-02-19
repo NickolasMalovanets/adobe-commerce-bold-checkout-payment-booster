cfaec04883e7d56e6ab3c6c0002ee3ce961126dd
Nickolas Malovanets <nickolas.malovanets@boldcommerce.com>
2025-02-19 16:02:32 +0200

    Hyvä with PB2.0 and Fastlane on M2.

--- view/frontend/web/hyva/js/payment-sdk-event-listener.js
+++ view/frontend/web/hyva/js/payment-sdk-event-listener.js
@@ -7,15 +7,12 @@ export function subscribeToSpiEvents() {
         }
         switch (eventType) {
             case 'EVENT_SPI_PAYMENT_ORDER_SCA':
-                console.log(eventType);
                 hyvaCheckout.loader.stop();
                 break;
             case 'EVENT_SPI_ENABLE_FULLSCREEN':
-                console.log(eventType);
                 hyvaCheckout.loader.stop();
                 break;
             case 'EVENT_SPI_DISABLE_FULLSCREEN':
-                console.log(eventType);
                 hyvaCheckout.loader.start();
                 break;
         }
